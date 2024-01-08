b922aa757583e5607947da07f2d2013664c85ac9
natsukashiixo <jesper.varg.viking@gmail.com>
2024-01-08 12:38:18 +0100

    created a skeleton

--- ntsuwrap/api_wrap/ep_channelslist.py
+++ ntsuwrap/api_wrap/ep_channelslist.py
@@ -3,46 +3,6 @@ import googleapiclient.discovery
 from ntsuwrap import YoutubeTokenBucket
 from youtube_status import youtube_status
 
-def get_subscribers(channel_id: str, API_KEY: str) -> int or str:
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "0"
-
-    api_service_name = "youtube"
-    api_version = "v3"
-    DEVELOPER_KEY = API_KEY
-
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, developerKey = DEVELOPER_KEY)
-
-    request = youtube.channels().list(
-        part="statistics",
-        id=channel_id
-    )
-    raw_response = request.execute()
-
-    if raw_response['items'][0]['statistics']['hiddenSubscriberCount'] == True:     #unsure about this but I think a raw "if" just checks if the element exists or not
-        return "HiddenFromView"
-    else:
-        subcount = int(raw_response['items'][0]['statistics']['subscriberCount'])
-        return subcount
-    
-def getall_channels_dot_list(channel_id: str, API_KEY: str, *args) -> tuple:
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "0"
-
-    api_service_name = "youtube"
-    api_version = "v3"
-    DEVELOPER_KEY = API_KEY
-
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, developerKey = DEVELOPER_KEY)
-
-    request = youtube.channels().list(
-        part="snippet,statistics,contentDetails,topicDetails,status,brandingSettings,localizations", # these are all the public parts
-        id=channel_id, #p sure this takes a csv string of max 50 elements
-        # but most likely I won't put in logic for handling multiple channels in this function.
-        maxResults=50
-    )
-    raw_response = request.execute()
-
 class ChannelsDotListSingle:
     def __init__(self, api_key: str, channel_id: str, bucket: YoutubeTokenBucket):
         # these should basically never change between classes
@@ -72,9 +32,30 @@ class ChannelsDotListSingle:
         if youtube_status() and self.bucket.take(self.TOKENCOST):
             self.yt_response = _get_response()
 
+    #items[0]snippet
+    def get_name(response):
+        pass
+    def get_desc(response):
+        pass
+    def get_url(response):
+        pass
+    def get_createtime(response):
+        pass #needs str to timestamp conversion
+    def get_pfp(response):
+        pass #use constructors to decide which one to get?
+        #like object.get_pfp(resp).high ?
+    
+    #items[0]statistics
     def get_subscribers(response) -> str or int:
         if response['items'][0]['statistics'].get('hiddenSubscriberCount') == True:     #unsure about this but I think a raw "if" just checks if the element exists or not
             return "HiddenFromView"
         else:
             subcount = int(response['items'][0]['statistics'].get('subscriberCount', 'N/A'))
         return subcount
+    def get_vidcount(response):
+        pass
+
+    #items[0]status
+    def get_privacystatus(response):
+        pass
+    
