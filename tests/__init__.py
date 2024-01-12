f669a2a2dbd86a813193646f66916f30132efbb7
natsukashiixo <jesper.varg.viking@gmail.com>
2024-01-12 14:38:26 +0100

    no idea how to write inits

---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+from ntsuwrap import YoutubeTokenBucket
+from ntsuwrap import ChannelsDotList
+from ntsuwrap import ParseChannels
+from ntsuwrap import PlaylistItemsDotList
+from ntsuwrap import ParsePlaylistItem
+from ntsuwrap import VideosDotList
+from ntsuwrap import ParseVideoItem
