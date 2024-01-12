601989c78f9536b906f8d48c28ec41f89c19c25d
natsukashiixo <jesper.varg.viking@gmail.com>
2024-01-12 13:43:54 +0100

    update thumbnail functions

--- ntsuwrap/api_wrap/ep_playlistitemslist.py
+++ ntsuwrap/api_wrap/ep_playlistitemslist.py
@@ -5,38 +5,6 @@ import pytz
 from ntsuwrap import YoutubeTokenBucket
 from youtube_status import youtube_status
 
-def relay_from_playlist(playlist_id: str, API_KEY: str) -> list | None:
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "0"
-
-    api_service_name = "youtube"
-    api_version = "v3"
-    DEVELOPER_KEY = API_KEY
-
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, developerKey = DEVELOPER_KEY)
-
-    request = youtube.playlistItems().list(
-        part="snippet,contentDetails",
-        playlistId=playlist_id,
-        maxResults=50
-    )
-    raw_response = request.execute()
-    
-    #print(raw_response)
-
-    if not raw_response.get('nextPageToken'):
-        videoID_list = []
-        for item in raw_response['items']:
-            #from pprint import pprint
-            #pprint(item, indent=4)
-            vid_id = item['contentDetails']['videoId']
-            videoID_list.append(vid_id)
-        return videoID_list
-    else:
-        print('Relay is currently out of scope of the project. Exiting')
-        return      # No time to implement today, relay starts tomorrow
-    
-
 class PlaylistItemsDotList:
     def __init__(self, api_key: str, playlist_id: str, bucket: YoutubeTokenBucket):
         # these should basically never change between classes
@@ -114,9 +82,8 @@ class ParsePlaylistItem:
     def get_vid_desc(self) -> str:
         return self.item_dict['snippet'].get('description')
         
-    def get_vid_thumbnails(self) -> str:
-        return self.item_dict['snippet'].get('thumbnails')
-         #decorator magic once again
+    def get_vid_thumbnails(self, quality='default' or 'medium' or 'high' or 'standard' or 'maxres') -> str:
+        return self.item_dict['snippet'].get('thumbnails').get(quality)
     
     def get_playlist_owner(self) -> str:
         return self.item_dict['snippet'].get('channelTitle')
@@ -128,8 +95,8 @@ class ParsePlaylistItem:
         return self.item_dict['snippet'].get('videoOwnerChannelId')
         
     def get_vid_index(self) -> int:
+        '''placement in playlist, starts at index 0'''
         return self.item_dict['snippet'].get('position')
-         #placement in playlist, starts at index 0
     
     def get_vid_id(self) -> str:
         return self.item_dict['snippet'].get('resourceId').get('videoID')
--- ntsuwrap/api_wrap/ep_videoslist.py
+++ ntsuwrap/api_wrap/ep_videoslist.py
@@ -71,16 +71,9 @@ class ParseVideoItem: #might only need this one and then do some checks if peopl
     def get_desc(self) -> str:
         return self.item_dict['snippet'].get('description')
     
-    def get_thumbnail(self) -> str:
-        '''object
-        Valid key values are:
-        default – The default thumbnail image. The default thumbnail for a video – or a resource that refers to a video, such as a playlist item or search result – is 120px wide and 90px tall. The default thumbnail for a channel is 88px wide and 88px tall.
-        medium – A higher resolution version of the thumbnail image. For a video (or a resource that refers to a video), this image is 320px wide and 180px tall. For a channel, this image is 240px wide and 240px tall.
-        high – A high resolution version of the thumbnail image. For a video (or a resource that refers to a video), this image is 480px wide and 360px tall. For a channel, this image is 800px wide and 800px tall.
-        standard – An even higher resolution version of the thumbnail image than the high resolution image. This image is available for some videos and other resources that refer to videos, like playlist items or search results. This image is 640px wide and 480px tall.
-        maxres – The highest resolution version of the thumbnail image. This image size is available for some videos and other resources that refer to videos, like playlist items or search results. This image is 1280px wide and 720px tall.'''
-        return self.item_dict['snippet'].get('thumbnails')
-     #use decorators or something to indicate quality
+    def get_thumbnail(self, quality='default' or 'medium' or 'high' or 'standard' or 'maxres') -> str:
+        return self.item_dict['snippet'].get('thumbnails').get(quality)
+        #i actually just want to be able to call an attribute of it like resp.get_thumbnail().maxres but i have no idea how to write that and it possibly involves subclassing so this hack will have to do
     
     def get_ch_id(self) -> str:
         return self.item_dict['snippet'].get('channelId')
