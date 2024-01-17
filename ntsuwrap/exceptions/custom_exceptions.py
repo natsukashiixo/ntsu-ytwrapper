80c0c38ecbef1ce8596754c50d3986ea44e1ce6d
natsukashiixo <jesper.varg.viking@gmail.com>
2024-01-17 13:17:48 +0100

    remove some sample code

--- ntsuwrap/exceptions/custom_exceptions.py
+++ ntsuwrap/exceptions/custom_exceptions.py
@@ -5,15 +5,3 @@ class EmptyResponseError(Exception):
 class PartialResponseError(Exception):  #Or maybe KeyError/ValueError?
     '''Raise if nested dictionary or list are empty'''
     pass
-
-def exception_usecase(_dict: dict, key: str):
-    try:
-        if not _dict == {} and not _dict.get(key) == None:
-            thing = _dict.get(key)
-            return thing
-        else:
-            raise PartialResponseError
-    except PartialResponseError as e:
-        print(f'Something went wrong trying to access {key} at {_dict}, defaulting to None')
-        log(e)
-        return None
