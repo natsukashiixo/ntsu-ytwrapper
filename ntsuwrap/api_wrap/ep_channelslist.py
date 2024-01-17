c1d3a9d26df5aecf599e0c5d13537e028a7ebc8d
natsukashiixo <jesper.varg.viking@gmail.com>
2024-01-17 13:16:56 +0100

    raise exceptions plus new parsing function

--- ntsuwrap/api_wrap/ep_channelslist.py
+++ ntsuwrap/api_wrap/ep_channelslist.py
@@ -4,6 +4,8 @@ from typing import Any
 import googleapiclient.discovery
 import pytz
 from ntsuwrap import YoutubeTokenBucket
+from ntsuwrap import EmptyResponseError
+from ntsuwrap import PartialResponseError
 from youtube_status import youtube_status
 
 class ChannelsDotList:
@@ -33,22 +35,41 @@ class ChannelsDotList:
 
         if youtube_status() and self.bucket.take(self.TOKENCOST):
             self.yt_response = _get_response()
+            if not self.yt_response:
+                raise EmptyResponseError
     
     def first_item(self) -> dict:
-        return self.yt_response['items'][0] if self.yt_response else {}
+        if not self.yt_response['items'] == []:
+            if not self.yt_response['items'][0] == {}:
+                return self.yt_response['items'][0]
+            raise PartialResponseError
+        raise EmptyResponseError
 
     def item_by_index(self, x: int) -> dict:
-        return self.yt_response['items'][x] if self.yt_response else {}
+        '''You should only do this if you legit know the index'''
+        if not self.yt_response['items'] == []:
+            if not self.yt_response['items'][x] == {}:
+                return self.yt_response['items'][x]
+            raise PartialResponseError
+        raise EmptyResponseError
 
     def item_by_snippet_kwp(self, keyword: str, value) -> dict:
-        for i, item in enumerate(self.yt_response.get('items', [])):
-            if item.get('snippet', {}).get(keyword) == value:
-                return item
-        print(f'key value pair {keyword}: {value} not found')
-        return {}
+        '''This is very static and potentially dangerous.
+        Please take care when using. A simple typo could make it return None.'''
+        if not self.yt_response['items'] == []:
+            for i, item in enumerate(self.yt_response.get('items')):
+                if item.get('snippet') == {}:
+                    raise PartialResponseError
+                if item.get('snippet').get(keyword) == value:
+                    return item
+            print(f'key value pair {keyword}: {value} not found')
+            return None
+        raise EmptyResponseError
 
     def all_items(self) -> list:
-        return self.yt_response.get('items', [])
+        if not self.yt_response['items'] == []:
+            return self.yt_response['items']
+        raise EmptyResponseError
 
 
 class ParseChannels:
@@ -93,3 +114,9 @@ class ParseChannels:
     #items[0]status
     def get_privacystatus(self) -> str:
         return self.item_dict['status'].get('privacyStatus')
+    
+    #items[0]contentDetails
+    def get_ch_uploads_playlist_id(self) -> str:
+        '''Its kinda weird, but to get the videos a channel has uploaded you have to use this to get a playlist ID. Then you can use this ID in the PlaylistItems endpoint to go through a channels uploaded videos.'''
+        return self.item_dict['contentDetails'].get('relatedPlaylists').get('uploads')
+
