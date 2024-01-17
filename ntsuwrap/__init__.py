1f35d549dd1ce5e29b1b68b5d7fbad3540f627c5
natsukashiixo <jesper.varg.viking@gmail.com>
2024-01-17 13:17:04 +0100

    import exceptions

--- ntsuwrap/__init__.py
+++ ntsuwrap/__init__.py
@@ -6,6 +6,10 @@ from .api_wrap.ep_videoslist import VideosDotList, ParseVideoItem
 from .api_wrap.ep_playlistitemslist import PlaylistItemsDotList, ParsePlaylistItem
 from .api_wrap.ep_channelslist import ChannelsDotList, ParseChannels
 
+#exceptions
+from .exceptions.custom_exceptions import EmptyResponseError
+from .exceptions.custom_exceptions import PartialResponseError
+
 #put whatever i name the data saving/transformation modules here
 
 #put stuff for tests here if needed
